c53mike/packer

move GeneratedData struct and PlaceholderMsg to same package under common

builder/amazon/chroot/step_mount_device.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/packer/builder"
 	"github.com/hashicorp/packer/common"
+	"github.com/hashicorp/packer/common/gendata"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 	"github.com/hashicorp/packer/template/interpolate"
@@ -31,7 +31,7 @@ type StepMountDevice struct {
 	MountPartition string
 
 	mountPath     string
-	GeneratedData *builder.GeneratedData
+	GeneratedData *gendata.GeneratedData
 }
 
 func (s *StepMountDevice) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {

builder/amazon/chroot/step_prepare_device.go
@@ -6,14 +6,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/hashicorp/packer/builder"
+	"github.com/hashicorp/packer/common/gendata"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 )
 
 // StepPrepareDevice finds an available device and sets it.
 type StepPrepareDevice struct {
-	GeneratedData *builder.GeneratedData
+	GeneratedData *gendata.GeneratedData
 }
 
 func (s *StepPrepareDevice) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {

builder/amazon/common/interpolate_build_info_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/packer/builder"
+	"github.com/hashicorp/packer/common/gendata"
 	"github.com/hashicorp/packer/helper/multistep"
 )
 
@@ -35,8 +35,8 @@ func testState() multistep.StateBag {
 	return state
 }
 
-func testGeneratedData(state multistep.StateBag) builder.GeneratedData {
-	generatedData := builder.GeneratedData{State: state}
+func testGeneratedData(state multistep.StateBag) gendata.GeneratedData {
+	generatedData := gendata.GeneratedData{State: state}
 	return generatedData
 }
 

outscale/packer-plugin-outscale

builder/amazon/chroot/step_mount_device.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/packer/builder"
 	"github.com/hashicorp/packer/common"
+	"github.com/hashicorp/packer/common/packerbuilderdata"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 	"github.com/hashicorp/packer/template/interpolate"
@@ -31,7 +31,7 @@ type StepMountDevice struct {
 	MountPartition string
 
 	mountPath     string
-	GeneratedData *builder.GeneratedData
+	GeneratedData *packerbuilderdata.GeneratedData
 }
 
 func (s *StepMountDevice) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {

builder/amazon/chroot/step_prepare_device.go
@@ -6,14 +6,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/hashicorp/packer/builder"
+	"github.com/hashicorp/packer/common/packerbuilderdata"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 )
 
 // StepPrepareDevice finds an available device and sets it.
 type StepPrepareDevice struct {
-	GeneratedData *builder.GeneratedData
+	GeneratedData *packerbuilderdata.GeneratedData
 }
 
 func (s *StepPrepareDevice) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {

builder/amazon/common/interpolate_build_info_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/packer/builder"
+	"github.com/hashicorp/packer/common/packerbuilderdata"
 	"github.com/hashicorp/packer/helper/multistep"
 )
 
@@ -35,8 +35,8 @@ func testState() multistep.StateBag {
 	return state
 }
 
-func testGeneratedData(state multistep.StateBag) builder.GeneratedData {
-	generatedData := builder.GeneratedData{State: state}
+func testGeneratedData(state multistep.StateBag) packerbuilderdata.GeneratedData {
+	generatedData := packerbuilderdata.GeneratedData{State: state}
 	return generatedData
 }
 
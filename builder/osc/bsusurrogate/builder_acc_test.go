4c370ec4cbe73fdfaa36c591573279a3c41d2d6f
Maxime Dufour <maxime.dufour@outscale.com>
2021-10-05 12:15:09 +0000

    Add tests for these fixes
    - builder => reject when shutdown_behavior is terminate and
    delete_on_vm_deletion is true
    - acc => test that the snapshot is performed when the
    delete_on_vm_deletion is terminate

--- builder/osc/bsusurrogate/builder_acc_test.go
+++ builder/osc/bsusurrogate/builder_acc_test.go
@@ -23,6 +23,21 @@ func TestAccBuilder_basic(t *testing.T) {
 	}
 	acctest.TestPlugin(t, testCase)
 }
+func TestAccBuilder_VmTerminate(t *testing.T) {
+	testCase := &acctest.PluginTestCase{
+		Name:     "bsusurrogate_vm_terminate_test",
+		Template: testBuilderAccVmTerminate,
+		Check: func(buildCommand *exec.Cmd, logfile string) error {
+			if buildCommand.ProcessState != nil {
+				if buildCommand.ProcessState.ExitCode() != 0 {
+					return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
+				}
+			}
+			return nil
+		},
+	}
+	acctest.TestPlugin(t, testCase)
+}
 
 const testBuilderAccBasic = `
 {
@@ -51,6 +66,38 @@ const testBuilderAccBasic = `
 			"volume_type": "standard"
 		}
 
+	}]
+}
+`
+
+const testBuilderAccVmTerminate = `
+{
+	"builders": [{
+		"type": "osc-bsusurrogate",
+		"region": "eu-west-2",
+		"vm_type": "t2.micro",
+		"source_omi": "ami-6c274bbe",
+		"ssh_username": "outscale",
+		"omi_name": "packer-test {{timestamp}}",
+		"omi_virtualization_type": "hvm",
+		"subregion_name": "eu-west-2a",
+		"launch_block_device_mappings" : [
+			{
+			"volume_type" : "io1",
+			"device_name" : "/dev/xvdf",
+			"delete_on_vm_deletion" : false,
+			"volume_size" : 10,
+			"iops": 300
+			}
+		],
+		"omi_root_device":{
+			"source_device_name": "/dev/xvdf",
+			"device_name": "/dev/sda1",
+			"delete_on_vm_deletion": true,
+			"volume_size": 10,
+			"volume_type": "standard"
+		},
+		"shutdown_behavior": "terminate"
 	}]
 }
 `

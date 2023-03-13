9c2bc3046db75d40567cf9bbbd45dfe3600a7e93
outscale_hmi <hanen.mizouni@outscale.com>
2023-03-13 13:13:03 +0000

    add datasource to output all OMI informations
    Signed-off-by: outscale_hmi <hanen.mizouni@outscale.com>

---
+++ datasource/omi/data_test.go
@@ -0,0 +1,53 @@
+package omi
+
+import (
+	"testing"
+
+	"github.com/hashicorp/packer-plugin-sdk/template/config"
+	"github.com/outscale/packer-plugin-outscale/builder/osc/common"
+)
+
+func TestDatasourceConfigure_FilterBlank(t *testing.T) {
+
+	datasource := Datasource{
+		config: Config{
+			OmiFilterOptions: common.OmiFilterOptions{},
+		},
+	}
+	if err := datasource.Configure(nil); err == nil {
+		t.Fatalf("Should error if filters map is empty or not specified")
+	}
+}
+
+func TestRunConfigPrepare_SourceOmiFilterOwnersBlank(t *testing.T) {
+	datasource := Datasource{
+		config: Config{
+			OmiFilterOptions: common.OmiFilterOptions{
+				NameValueFilter: config.NameValueFilter{
+					Filters: map[string]string{"foo": "bar"},
+				},
+			},
+		},
+	}
+	if err := datasource.Configure(nil); err == nil {
+		t.Fatalf("Should error if Owners is not specified)")
+	}
+}
+
+func TestRunConfigPrepare_SourceOmiFilterGood(t *testing.T) {
+	filter_key := "name"
+	filter_value := "foo"
+	datasource := Datasource{
+		config: Config{
+			OmiFilterOptions: common.OmiFilterOptions{
+				NameValueFilter: config.NameValueFilter{
+					Filters: map[string]string{filter_key: filter_value},
+				},
+				Owners: []string{"1234567"},
+			},
+		},
+	}
+	if err := datasource.Configure(nil); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
